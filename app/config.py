2274a2066a4d7bebc8617fcb6bb1b896708df787
Sang Han <sanghan@protonmail.com>
2024-07-29 18:54:12 -0700

    make cap3d default captions

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    CAPTIONS_FILE: str = "data/pali_captions_with_likes.csv"
+    CAPTIONS_FILE: str = "data/cap3d_captions_with_likes.csv"
     EMBEDDINGS_FILE: str = "data/embeddings.npy"
     DATABASE_PATH: str = "data/database.sqlite3"
     SENTENCE_TRANSFORMER_MODEL: str = "all-MiniLM-L6-v2"
