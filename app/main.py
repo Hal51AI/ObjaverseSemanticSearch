1a2583bd19e906cba0ebaec8a1aeb41657d65950
Sang Han <sanghan@protonmail.com>
2024-09-20 12:11:34 -0700

    add cors middleware

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from textwrap import dedent
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .config import settings
 from .db import create_db
@@ -45,6 +46,14 @@ app = FastAPI(
     contact={"name": "Hal51 AI", "url": "https://github.com/hal51ai"},
     license_info={"name": "MIT LIcense", "identifier": "MIT"},
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(similarity.router, prefix="/similarity", tags=["similarity"])
